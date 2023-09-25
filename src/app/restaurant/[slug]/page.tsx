eeb8bd0a8bad1c8b1375ba9cf5397acc4573921f
Valon Haxhidauti <valon@gjirafa.com>
2023-09-26 00:14:23 +0200

    commit

--- src/app/restaurant/[slug]/page.tsx
+++ src/app/restaurant/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import Header from "./components/Header";
 import RestaurantNavBar from "./components/RestaurantNavBar";
 import Title from "./components/Title";
 import Rating from "./components/Rating";
@@ -6,17 +5,55 @@ import Description from "./components/Description";
 import Images from "./components/Images";
 import Reviews from "./components/Reviews";
 import ReservationCard from "./components/ReservationCard";
+import { PrismaClient } from "@prisma/client";
 
-export default function RestaurantDetails() {
+interface Restaurant {
+  slug: string;
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+}
+
+const prisma = new PrismaClient();
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
+  });
+
+  if (!restaurant) {
+    throw new Error();
+  }
+
+  return restaurant;
+};
+
+export default async function RestaurantDetails({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+
   return (
     <>
       <title>Milestones Gril (Toronto) | OpenTable</title>
       <div className="bg-white w-[70%] rounded p-3 shadow">
-        <RestaurantNavBar />
-        <Title />
+        <RestaurantNavBar slug={restaurant.slug} />
+        <Title name={restaurant.name} />
         <Rating />
-        <Description />
-        <Images />
+        <Description description={restaurant.description} />
+        <Images images={restaurant.images} />
         <Reviews />
       </div>
       <div className="w-[27%] relative text-reg">
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,8 +1,51 @@
 import Header from "./components/Header";
 import SearchSideBar from "./components/SearchSideBar";
 import RestaurantCard from "./components/RestaurantCard";
+import { PrismaClient } from "@prisma/client";
 
-export default function Search() {
+const prisma = new PrismaClient();
+
+const fetchRestaurantByCity = (city: string | undefined) => {
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    cuisine: true,
+    location: true,
+    slug: true,
+  };
+
+  if (!city) return prisma.restaurant.findMany({ select });
+
+  return prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city.toLowerCase(),
+        },
+      },
+    },
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      price: true,
+      cuisine: true,
+      location: true,
+      slug: true,
+    },
+  });
+};
+
+export default async function Search({
+  searchParams,
+}: {
+  searchParams: { city: string };
+}) {
+  const restaurants = await fetchRestaurantByCity(searchParams.city);
+
+  console.log(restaurants);
   return (
     <>
       <title>Search Restaurants OpenTable</title>
@@ -10,7 +53,11 @@ export default function Search() {
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
         <SearchSideBar />
         <div className="w-5/6">
-          <RestaurantCard />
+          {restaurants.length ? (
+            <RestaurantCard />
+          ) : (
+            <p>Sorry, we found no restaurants in this area</p>
+          )}
         </div>
       </div>
     </>
